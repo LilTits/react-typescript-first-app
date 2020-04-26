17e7a3ef5689031409906b8f9091d2db81da351f
LilTits <titouan@guiochet.com>
2020-04-26 09:08:39 +0200

    Update App.tsx to include API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const APP_ID = "a70194b6"
+  const APP_KEY = "90cce5be20bee1f1f1c276272ded8158"
+
+  const query = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    get_recipes_2()
+  })
+
+  //const get_recipes = () => {
+  //  const resp = await fetch(query);
+  //  const data = repsonse.json();
+  //  console.log(data)
+  //}
+
+  const get_recipes_2 = () => {
+    fetch(query).then(
+      resp => {
+        resp.json()
+      }
+    )
+  }
+
+  return(
+    <div className="className">
+      <form className="search-form">
+        <input className="search-input" type="text"/>
+        <button className="search-button" type="submit">
+          {counter}
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
